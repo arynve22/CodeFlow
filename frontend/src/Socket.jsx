b08442168606be3da4d41595e6c2cc4e4a466a06
Aryan <aryanve2207@gmail.com>
2025-11-26 02:24:00 +0530

    initial commit

---
+++ frontend/src/Socket.jsx
@@ -0,0 +1,15 @@
+import {io} from 'socket.io-client';
+
+export const initSocket = async () =>{
+    const options = {
+        'force new connection': true,
+        reconnectionAttempts : 'Infinity',
+        timeout: 10000,
+        transports: ['websocket'],
+    };
+    return io(import.meta.env.VITE_BACKEND_URL, options, {
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 500
+});
+}
